536db5bd5e76db21efa5644548c06e31717e771b
Carlos-SSH <carlos.anezeh@me.com>
2023-10-27 23:51:17 -0600

    Add tests to InvoiceItems as a validation relationship

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,6 +12,15 @@ RSpec.describe Invoice, type: :model do
       invoice = customer.invoices.create!(customer_id: 1, sub_total: 200, tax: 16, total: 1200)
 
       expect(invoice.created_at?).to eq(true)
+      expect(invoice.customer_id).to_not eq(nil)
+      expect(invoice.sub_total).to_not eq(nil)
+      expect(invoice.tax).to_not eq(nil)
+      expect(invoice.total).to_not eq(nil)
+
+      expect(invoice.customer_id).to_not eq("text")
+      expect(invoice.sub_total).to_not eq("text")
+      expect(invoice.tax).to_not eq("text")
+      expect(invoice.total).to_not eq("text")
     end
   end
 end
