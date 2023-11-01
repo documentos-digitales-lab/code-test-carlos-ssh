7573fafa184eb402739d3752ff4d40f1498eda55
Carlos-SSH <carlos.anezeh@me.com>
2023-10-31 18:01:41 -0600

    Fix Rubocop issues

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -11,21 +11,20 @@ RSpec.describe Customer, type: :model do
     it { should have_many :invoices }
   end
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     customer = Customer.new(rfc: 'AAA')
     expect(customer).to be_valid
   end
 
-  it "is not valid without an RFC" do
+  it 'is not valid without an RFC' do
     customer = Customer.new
     expect(customer).not_to be_valid
     expect(customer.errors[:rfc]).to include("can't be blank")
   end
 
-  it "is not valid with an RFC of incorrect length" do
+  it 'is not valid with an RFC of incorrect length' do
     customer = Customer.new(rfc: 'A')
     expect(customer).not_to be_valid
-    expect(customer.errors.messages[:rfc]).to include("is too short (minimum is 3 characters)")
+    expect(customer.errors.messages[:rfc]).to include('is too short (minimum is 3 characters)')
   end
-
 end
